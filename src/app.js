f84a92b59210615672fa2ebaa369724f250b1bda
mayankrajoriya <149350368+mayankrajoriya@users.noreply.github.com>
2024-07-13 12:45:23 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@ const express = require("express");
 const app = express();
 
 const subscriberModel = require("../models/subscribers");
-const userModel = require("../models/user-model");
-const { generateToken } = require("../utils/generatetoken");
-const isLoggedIn = require("../middleware/isLoggedIn");
+
 
 const path = require("path");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
@@ -25,92 +22,18 @@ app.get("/", (req, res) => {
 });
 
 
-//user register
-
-
-app.post("/create", async (req, res) => {
-  try{
-    
-    const registeredUser = await userModel.findOne({
-      email: req.body.email,
-    });
-  
-    if (registeredUser) {
-      res.status(403).send("you are already registered");
-    } else {
-      let { fullname, email, password } = req.body;
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt, async (err, hash) => {
-          const user = await userModel.create({
-            fullname,
-            email,
-            password: hash,
-          });
-          const token = generateToken(user);
-          res.cookie("token", token);
-          res.redirect("/home");
-        });
-      });
-    }
-
-
-  }catch(error){
-    res.status(400).send("server error",error)
-  }
-});
-
-//user logout
-
-app.get("/logout", (req, res) => {
-  res.cookie("token", "");
-  res.redirect("/");
-});
-
-//user login
-
-app.post("/login", async (req, res) => {
- try{
-  let { email, password } = req.body;
-
-  const user = await userModel.findOne({
-    email,
-  });
-  if (!user) {
-    res.status(401).send("something went wrong plz try again");
-  } else {
-    bcrypt.compare(password, user.password, (err, result) => {
-      if (!result) {
-        res.status(401).send("something went wrong");
-      } else {
-        const token = generateToken(user);
-        res.cookie("token", token);
-        res.redirect("/home");
-      }
-    });
-  }
- }catch(error){
-  res.status(400).send("server error",error)
- }
-});
-
-//home page
-
-app.get("/home", isLoggedIn,async (req, res) => {
-  
-  res.render("home");
-});
 
 //subscribers api
 
-app.get("/subscribers", isLoggedIn, async (req, res) => {
+app.get("/subscribers", async (req, res) => {
   const subscribers = await subscriberModel.find();
   res.json(subscribers);
 });
 
-app.get("/subscriber/create",isLoggedIn, (req, res) => {
+app.get("/subscriber/create", (req, res) => {
   res.render("subscriber-create");
 });
-app.post("/subscribercreate", isLoggedIn, async (req, res) => {
+app.post("/subscribercreate", async (req, res) => {
   try{
     let { name, subscribedChannel } = req.body;
   const subscriberUser = await subscriberModel.create({
@@ -127,7 +50,7 @@ app.post("/subscribercreate", isLoggedIn, async (req, res) => {
 
 //subscribers/names api
 
-app.get("/subscribers/names", isLoggedIn, async (req, res) => {
+app.get("/subscribers/names",  async (req, res) => {
   const subscribers = await subscriberModel
     .find()
     .select("-_id")
@@ -144,7 +67,8 @@ app.post('/subscribersid',async (req, res) => {
 });
 
 
-app.get("/subscribers/:id",isLoggedIn, async (req, res) => {
+
+app.get("/subscribers/:id", async (req, res) => {
   const subscriber = await subscriberModel.findOne({
     _id: req.params.id,
   });
--- utils/generatetoken.js
+++
@@ -1,8 +0,0 @@
-const jwt=require("jsonwebtoken");
-
-const generateToken=(user)=>{
-    return jwt.sign({email:user.email,id:user._id},"uniqueKey")
-}
-
-
-module.exports.generateToken=generateToken
